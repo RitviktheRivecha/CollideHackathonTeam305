a4b2d12fe7b2441db7123878465033ff44dbebc6
RitviktheRivecha <rchilakamarthy@gmail.com>
2024-09-26 20:36:24 -0500

    changed name

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+#COLLIDEHACKATHON TEAM #305
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -11,6 +13,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('COLLIDE HACKATHON TEAM 305')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
